b01b43b1762b3193ba115ae3a130ee01502adf9b
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-06-16 16:27:58 +0000

    16-06-2023 Done

---
+++ Day4/5-LongestSubstring.cpp
@@ -0,0 +1,37 @@
+/* Sliding Window Approach
+  Take two pointers (start and end) initially both at 0th index and a
+  set which will be storing the characters between start and end index.
+
+   Time Complexity : O(N)
+    Space Complexity : O(K)
+
+    where N is the length of input string and
+    K is the number of unique characters in the string
+*/
+#include <unordered_map>
+
+int uniqueSubstrings(string input) 
+{
+    int n = input.length();
+
+    unordered_map<char, int> set;
+
+    int ans = 0, i = 0, j = 0;
+
+    while (i < n && j < n) 
+    {
+        // Try to extend the range [i,j]
+        if (set.find(input[j]) == set.end()) 
+        {
+            set[input[j]] = 1;
+            ans = max(ans, j - i + 1);
+            j++;
+        } else 
+        {
+            set.erase(input[i]);
+            i++;
+        }
+    }
+
+    return ans;
+}
