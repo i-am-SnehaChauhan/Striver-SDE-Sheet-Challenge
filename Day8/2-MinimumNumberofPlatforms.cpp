8abbe49c497eea83549c3220eb6b8b1297b0dee2
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-05 22:21:24 +0530

    Done

---
+++ Day8/2-MinimumNumberofPlatforms.cpp
@@ -0,0 +1,7 @@
+/*Approach: 
+    Sort the arrival and departure time of trains.
+    If the arrival time is less than departure time then platform is needed.
+    Time Complexity: O(nlogn)
+    Space Complexity: O(1)
+
+*/
